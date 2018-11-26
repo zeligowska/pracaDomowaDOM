ec5f09c7ea94c0fb6e2c0f85b45e8cec0c7bc819
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2018-11-26 21:49:56 +0100

    second task v 1.0

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var counter = 0;
 var counterDOM = document.querySelector('#cnt');
 var buttonPlus = document.querySelector('.btn-plus');
 var buttonMinus = document.querySelector('.btn-minus');
+var toastDOM = document.querySelector('#toast');
 
 function updateCounterDOM() {
     counterDOM.innerHTML = counter;
@@ -11,7 +12,9 @@ buttonPlus.addEventListener(
     'click',
     () => {
         counter++;
+        toastDOM.innerHTML = 'You added 1';
         updateCounterDOM();
+        showToast();
     }
 )
 
@@ -19,8 +22,17 @@ buttonMinus.addEventListener(
     'click',
     () => {
         counter--;
+        toastDOM.innerHTML = 'You subtract 1';
         updateCounterDOM();
+        showToast();
     }
 )
+
+function showToast() {
+    toastDOM.className = 'show';
+    setTimeout(() => {
+        toastDOM.className = toastDOM.className.replace('show', '')
+    }, 1500);
+}
 
 updateCounterDOM();
